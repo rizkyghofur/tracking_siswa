cee71f9e68f133d5e31c763c02c0dc393296b2b0
rizkyghofur <rizky.abdulghofur@gmail.com>
2020-11-29 19:34:36 +0700

    Perubahan dasar tahap 1

--- application/controllers/Beranda.php
+++ application/controllers/Beranda.php
@@ -1,18 +1,16 @@
 <?php
 defined('BASEPATH') or exit('No direct script access allowed');
 
-class Login extends CI_Controller
+class Beranda extends CI_Controller
 {
 
   public function __construct()
   {
     parent::__construct();
-    $this->load->library('form_validation');
-    $this->load->model('login');
   }
 
   public function index()
   {
-    $this->load->view('login');
+    $this->load->view('beranda');
   }
 }
